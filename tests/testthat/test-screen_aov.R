a93da7ea407068342568e809ce5d6614eec12289
Mimi Wang <mimikwang@gmail.com>
2019-09-14 14:46:32 -0700

    add test coverage

--- tests/testthat/test-screen_aov.R
+++ tests/testthat/test-screen_aov.R
@@ -6,6 +6,8 @@ test_that("Normal", {
   expect_s3_class(out, "screen_aov")
   expect_equal(nrow(out$results), 2)
   expect_equal(ncol(out$results), 10)
+  expect_error(screen_aov(data.frame(x = as.numeric(), y = as.numeric()), "x", "y"), NA)
+  expect_error(print(screen_aov(mtcars, "cyl", "mpg")), NA)
 })
 
 # Test for throwing errors
@@ -13,9 +15,12 @@ test_that("Test for Throwing Errors", {
   # Incorrect Input Type
   expect_error(screen_aov(1, 1, 1))
   expect_error(screen_aov(c("sdf"), mtcars, mtcars))
+  expect_error(screen_aov(mtcars, mtcars, "cyl"))
+  expect_error(screen_aov(mtcars, "cyl", mtcars))
 
   # Responses and Factors not in DataFrame
   expect_error(screen_aov(mtcars, c("cyl", "mpg"), "asdf"))
+  expect_error(screen_aov(mtcars, "asdf", c("cyl", "mpg")))
 })
 
 # Test for Warnings
